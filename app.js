21f2e786239b372a44c597beebd57879470550f7
Preetangoud <preetangoud123@gmail.com>
2022-10-10 19:09:05 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const db = require('./db');
+const port = 5000;
+const cors = require('cors');
+app.use(cors());
+
+const authController = require('./controller/authController');
+app.use('/api/auth',authController)
+
+app.listen(port,() => {
+    console.log(`listening on port ${port}`)
+})
